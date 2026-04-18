c624a8e6d07196d2b7c4d6c1eede31540389fe4e
Harsh <yourdevharsh@gmail.com>
2026-04-18 17:03:30 +0530

    Created whole structure, partially completed styling and added feature changing theme.

--- Prompts.md
+++ Prompts.md
@@ -1,3 +1,9 @@
-## Prompt 1: 
+### Prompt 1: 
 {Project Details}
 I am choosing level 3. Describe all requirements in a list.
+
+### Prompt 2:
+List 10% of utility classes of Tailwind CSS used 90% of the time with their corresponding vanilla CSS.
+
+### Prompt 3:
+Which will be the best font for this website?
--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-import './style.css';
+import "./style.css";
+
+const themeIcon = document.getElementById("themeIcon");
+themeIcon.addEventListener("click", () => {
+  document.body.classList.toggle("darkTheme");
+  if (themeIcon.src.includes('sun.svg')) {
+    themeIcon.src = '/src/assets/moon.svg'
+  } else {
+    themeIcon.src = '/src/assets/sun.svg'
+  }
+});
