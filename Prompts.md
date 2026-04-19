863024c37563e3091e4a1ae92aafd7ce5533eeb1
Harsh <yourdevharsh@gmail.com>
2026-04-19 11:45:48 +0530

    Completed all the requirements of project, fine tuning is left.

--- Prompts.md
+++ Prompts.md
@@ -7,3 +7,6 @@ List 10% of utility classes of Tailwind CSS used 90% of the time with their corr
 
 ### Prompt 3:
 Which will be the best font for this website?
+
+### Prompt 4:
+How backdrop filter works in tailwind CSS?
--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
 import "./style.css";
 
 const themeIcon = document.getElementById("themeIcon");
+const cards = document.querySelectorAll(".card");
+const footer = document.getElementById('footer');
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
+
 themeIcon.addEventListener("click", () => {
-  document.body.classList.toggle("darkTheme");
-  if (themeIcon.src.includes('sun.svg')) {
-    themeIcon.src = '/src/assets/moon.svg'
+  if (themeIcon.src.includes("sun.svg")) {
+    themeIcon.src = "/src/assets/moon.svg";
   } else {
-    themeIcon.src = '/src/assets/sun.svg'
+    themeIcon.src = "/src/assets/sun.svg";
   }
+
+  document.documentElement.classList.toggle("dark");
+});
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+  menu.classList.toggle("translate-x-full");
 });
